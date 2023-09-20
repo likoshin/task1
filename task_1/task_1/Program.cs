c0c7a14d7553ec012c06f2b0ca8b2ead7b70be4e
likoshin <74928664+likoshin@users.noreply.github.com>
2023-09-20 08:38:28 +0300

    Add files via upload

---
+++ task_1/task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Слава Україні!!!");
+        }
+    }
+}
